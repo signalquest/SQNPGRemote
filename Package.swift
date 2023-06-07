6980b22ae567f951561cdb2e43239d3681b76b6e
Chris Scelza <cscelza@signalquest.com>
2023-06-07 10:50:51 -0400

    Relocated SQNPG binary target v1.3.0 for v1.3.0b release

--- Package.swift
+++ Package.swift
@@ -9,10 +9,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SQNPG",
-            targets: ["SQNPG"]),
+            targets: ["SQNPG"]
+        ),
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "SQNPG", url: "https://signalquest.net/sq-survey/packages/iOS/SQNPG/v1.3.2/SQNPG.xcframework.zip", checksum: "7c2f3b1942bc5f45b1c5f0576095c2b5f9c325bccdcea0e0e293c040f53d5b4e")
+        .binaryTarget(
+            name: "SQNPG",
+            url: "https://signalquest.net/sq-survey/packages/iOS/SQNPG/v1.3.0b/SQNPG.xcframework.zip",
+            checksum: "cefdf05b150b71250df7991ef0986e6272152c2baf64666d7f5755b8778e47d1"
+        )
     ]
 )
